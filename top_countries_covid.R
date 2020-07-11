bcc22963646333c75a8eb21f4a00d12b13474acc
roeungchoice <65836152+roeungchoice@users.noreply.github.com>
2020-07-11 18:48:56 -0400

    Update top_countries_covid.R

--- top_countries_covid.R
+++ top_countries_covid.R
@@ -1,3 +1,4 @@
+#import packages
 library(tidyverse)
 library(dplyr)
 library(readr)
@@ -6,10 +7,13 @@ library(gganimate)
 library(gifski)
 library(ggthemes)
 
+#read csv file
 data_by_country <- read_csv(file = 'owid-covid-data.csv')
 
+#select countries from data_by_country
 top_countries <- c('United States', 'China', 'Japan', 'United Kingdom', 'Germany', 'India', 'Australia', 'Canada', 'France', 'Italy')
 
+#creating graph 'p'
 p <- data_by_countries %>% filter(location %in% top_countries) %>%
   ggplot(aes(date, total_cases, color = location)) +
   geom_line() +
@@ -22,7 +26,8 @@ p <- data_by_countries %>% filter(location %in% top_countries) %>%
   theme(axis.title = element_text()) +
   facet_wrap(~ location)
 
+#animating graph 'p'
 p <- p + transition_reveal(date)
-#animate(p, height = 461, width = 644)
+animate(p, height = 461, width = 644)
 
 file_renderer(dir = ".", prefix = "gganim_plot", overwrite = FALSE)
